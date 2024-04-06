3f2217293aa3ae3e79d6e19ffe8a09b7333e9413
Gumalevskii Roman <gumalevskij@mail.ru>
2024-04-06 21:31:54 +0700

    restapi: add getLastComic

--- RESTAPI/pkg/xkcd/xkcd.go
+++ RESTAPI/pkg/xkcd/xkcd.go
@@ -12,7 +12,32 @@ type Comic struct {
 	Transcript string
 }
 
+func getLastComic(baseURL string) (Comic, error) {
+    url := fmt.Sprintf("%s/info.0.json", baseURL)
+    resp, err := http.Get(url)
+    if err != nil {
+        return Comic{}, err
+    }
+    defer resp.Body.Close()
+
+    var comic Comic
+    if err := json.NewDecoder(resp.Body).Decode(&comic); err != nil {
+        return Comic{}, err
+    }
+
+    return comic, nil
+}
+
 func FetchComics(baseURL string, limit int) ([]Comic, error) {
+	lastComic, err := getLastComic(baseURL)
+    if err != nil {
+        return nil, err
+    }
+
+	if limit < 0 || limit > lastComic.Num {
+        limit = lastComic.Num
+    }
+
 	var comics []Comic
 	for i := 1; i <= limit; i++ {
 		url := fmt.Sprintf("%s/%d/info.0.json", baseURL, i)

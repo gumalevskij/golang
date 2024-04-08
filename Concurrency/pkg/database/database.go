36823d8a0d30e1ec6b03d9efc3b5c02f2061f3bc
Gumalevskii Roman <gumalevskij@mail.ru>
2024-04-08 22:44:06 +0700

    concurrency: init

---
+++ Concurrency/pkg/database/database.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type NormalizedComic struct {
+	Url      string   `json:"url"`
+	Keywords []string `json:"keywords"`
+}
+
+type Comics map[string]NormalizedComic
+
+func LoadComics(path string) (Comics, error) {
+	var comics Comics
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return comics, nil
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, &comics)
+	if err != nil {
+		return nil, err
+	}
+
+	return comics, nil
+}
+
+func SaveComics(path string, comics Comics) error {
+	data, err := json.MarshalIndent(comics, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(path, data, 0644)
+}

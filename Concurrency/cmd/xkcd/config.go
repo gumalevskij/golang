084858355bd86ba9abb591080f61b8b0e624c818
Gumalevskii Roman <gumalevskij@mail.ru>
2024-04-09 11:22:39 +0700

    concurrency: move config in other file

---
+++ Concurrency/cmd/xkcd/config.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	SourceURL     string `yaml:"source_url"`
+	DbFile        string `yaml:"db_file"`
+	StopWordsFile string `yaml:"stopwords_file"`
+	Parallel      int    `yaml:"parallel"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	var config Config
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
